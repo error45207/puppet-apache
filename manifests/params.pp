1ded3c2717b26693b29aece071f4c1f7e39bec18
root <root@3d20a6b4361c.mylabserver.com>
2021-04-25 15:45:41 +0000

    refactored for debian in install class

---
+++ manifests/params.pp
@@ -0,0 +1,12 @@
+# @summary Params for apache module
+class apache::params {
+  $install_ensure = 'present'
+  case $::osfamily {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+  }
+}
